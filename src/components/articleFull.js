08107a5b0865dbf69985211b29be0c1f03e916fe
Radminas Usackas <usackasr@gmail.com>
2021-12-10 19:58:35 +0000

    Created articleFull component

---
+++ src/components/articleFull.js
@@ -0,0 +1,21 @@
+import MarkdownIt from "markdown-it";
+
+function ArticleFull({ article }) {
+    const mdParser = new MarkdownIt({
+        linkify: true        // Autoconvert URL-like text to links
+    });
+
+    return (
+        <div>
+            <div className="text-center">
+                <h1>{article.title}</h1>
+                <p>By {article.author}</p>
+            </div>
+            <hr />
+            <div dangerouslySetInnerHTML={{ __html: (mdParser.render(JSON.parse(article.article))) }}>
+            </div>
+        </div>
+    )
+}
+
+export default ArticleFull;
