332347474c3412c4cc11bebf1aed8419e355747c
Radminas Usackas <usackasr@gmail.com>
2021-12-12 22:21:38 +0000

    Updated About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,16 @@
 function About() {
     return (
-        <div className="text-center mt-3">
-            <h1>About</h1>
+        <div className="container-fluid text-center mt-3 mw">
+                <h1>About</h1>
+                <br />
+                <h3>Technologies used:</h3>
+                <ul className="list-group shadow">
+                    <li className="list-group-item">REST API</li>
+                    <li className="list-group-item">NodeJS Express</li>
+                    <li className="list-group-item">ReactJS</li>
+                    <li className="list-group-item">MongoDB</li>
+
+                </ul>
         </div>
     )
 }
