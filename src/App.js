85bf646848ce18e0204f41190b3b616bbb0eb4cb
Radminas Usackas <79939842+ne0rad@users.noreply.github.com>
2021-12-14 22:04:43 +0000

    Updated APP_URI

--- src/App.js
+++ src/App.js
@@ -12,30 +12,29 @@ import About from "./pages/About";
 import Article from "./pages/Article";
 import Home from "./pages/Home";
 import NewArticle from "./pages/NewArticle";
-import NotFound from "./pages/NotFound"
+import NotFound from "./pages/NotFound";
 
-const API_URI = process.env.REACT_APP_API_URI;
+const API_URI = "https://rocky-falls-75959.herokuapp.com";
 
 function App() {
-
   const [articles, setArticles] = useState();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (loading) {
-      fetch(API_URI + '/articles')
-        .then(response => {
+      fetch(API_URI + "/articles")
+        .then((response) => {
           if (response.ok) return response.json();
-          else throw new Error('Failed to fetch data.');
+          else throw new Error("Failed to fetch data.");
         })
-        .then(data => {
+        .then((data) => {
           setArticles(data);
           setLoading(false);
         })
         .catch((error) => {
           setArticles(error);
           setLoading(false);
-        })
+        });
     }
   }, [loading]);
 
@@ -43,17 +42,12 @@ function App() {
     setLoading(true);
   }
 
-
   function CustomLink({ children, to, ...props }) {
     let resolved = useResolvedPath(to);
     let match = useMatch({ path: resolved.pathname, end: true });
     return (
       <div>
-        <Link
-          style={match && { color: "white" }}
-          to={to}
-          {...props}
-        >
+        <Link style={match && { color: "white" }} to={to} {...props}>
           {children}
         </Link>
       </div>
@@ -65,7 +59,9 @@ function App() {
       <div>
         <nav className="navbar navbar-expand-md navbar-dark bg-primary bg-gradient shadow">
           <div className="container-fluid">
-            <Link className="navbar-brand" to="/">Blog App</Link>
+            <Link className="navbar-brand" to="/">
+              Blog App
+            </Link>
             <button
               className="navbar-toggler"
               type="button"
@@ -77,16 +73,25 @@ function App() {
             >
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
               <ul className="navbar-nav me-auto">
                 <li className="nav-item">
-                  <CustomLink className="nav-link" to="/">Home</CustomLink>
+                  <CustomLink className="nav-link" to="/">
+                    Home
+                  </CustomLink>
                 </li>
                 <li className="nav-item">
-                  <CustomLink className="nav-link" to="/new">New Article</CustomLink>
+                  <CustomLink className="nav-link" to="/new">
+                    New Article
+                  </CustomLink>
                 </li>
                 <li className="nav-item">
-                  <CustomLink className="nav-link" to="/about">About</CustomLink>
+                  <CustomLink className="nav-link" to="/about">
+                    About
+                  </CustomLink>
                 </li>
               </ul>
             </div>
@@ -95,15 +100,19 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home articles={articles} />} />
-          <Route path="/new" element={<NewArticle API_URI={API_URI} refreshArticles={refreshArticles} />} />
+          <Route
+            path="/new"
+            element={
+              <NewArticle API_URI={API_URI} refreshArticles={refreshArticles} />
+            }
+          />
           <Route path="/about" element={<About />} />
           <Route path="/articles/:id" element={<Article API_URI={API_URI} />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-
       </div>
     </HashRouter>
-  )
+  );
 }
 
 export default App;
